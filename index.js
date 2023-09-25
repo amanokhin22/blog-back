9f6a912b37b3db137d9722b4e8b76dced4e3f03d
Artiom Manokhin <amanokhin22@ukr.net>
2023-09-25 14:38:48 +0300

    fix

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ app.patch("/posts/:id", postCreateValidation, handleValidationErrors, PostContro
 
 
 app.listen(process.env.PORT || 4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
     console.log('Server Ok')
 });
 
